a66d59c969394f95cec398a5df313b505e0b4017
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-26 14:34:27 -0500

    commit tree 2ee850c6753736857a4c14104eff66e050fd7a4b

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    self.meals.map {|meal| meal.waiter}
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
+    attr_accessor :name, :years
+
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+        best_tipped_meal.customer
+    end
+
+    def frequent_customer
+        most_visits = 0
+        most_frequent = nil
+        self.all.each do |customer|
+            binding.pry
+        end
+    end
 
 end
